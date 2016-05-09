a58b9d3dc713c5ca209d47f786c6aad06caec3ac
Graham Blevins <hgblevins@gmail.com>
2016-05-09 16:24:42 -0400

    setup default hooks, version bump

--- Base.php
+++ Base.php
@@ -33,21 +33,22 @@ abstract class Base extends \Expresser\Support\Model {
     $this->role->remove_cap($capability);
   }
 
-  protected static function registerRole() {
+  public static function registerHooks($class) {
+
+    add_action('after_switch_theme', [$class, 'registerRole']);
+    add_action('after_switch_theme', [$class, 'registerCapabilities']);
+    add_action('switch_theme', [$class, 'deregisterCapabilities']);
+    add_action('switch_theme', [$class, 'deregisterRole']);
+  }
+
+  public static function registerRole() {
 
     $role = new static;
 
     add_role($role->name, $role->label);
   }
 
-  protected static function deregisterRole() {
-
-    $role = new static;
-
-    remove_role($role->name);
-  }
-
-  protected static function registerCapabilities() {
+  public static function registerCapabilities() {
 
     $role = new static;
 
@@ -57,7 +58,14 @@ abstract class Base extends \Expresser\Support\Model {
     }
   }
 
-  protected static function deregisterCapabilities() {
+  public static function deregisterRole() {
+
+    $role = new static;
+
+    remove_role($role->name);
+  }
+
+  public static function deregisterCapabilities() {
 
     $role = new static;
 
@@ -65,14 +73,6 @@ abstract class Base extends \Expresser\Support\Model {
 
       $role->removeCapability($capability);
     }
-  }
-
-  protected static function registerHooks($class) {
-
-    add_action('after_switch_theme', [$class, 'registerRole']);
-    add_action('after_switch_theme', [$class, 'registerCapabilities']);
-    add_action('switch_theme', [$class, 'deregisterCapabilities']);
-    add_action('switch_theme', [$class, 'deregisterRole']);
   }
 
   public abstract function label();
--- Native.php
+++ Native.php
@@ -2,12 +2,12 @@
 
 abstract class Native extends Base {
 
-  protected static function registerRole() {
+  public static function registerRole() {
 
     // Do not implement registerRole for native or exisitng roles
   }
 
-  protected static function deregisterRole() {
+  public static function deregisterRole() {
 
     // Do not implement deregisterRole for native or exisitng roles
   }
